bba44113e05f1de470d66c8ffd50a0957786a2a6
Ivan Andreevich <56831140+andreevich404@users.noreply.github.com>
2024-04-20 12:55:19 +0300

    fix some requirements

--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,9 @@
 const gamesRouter = require("express").Router();
-const { sendAllGames, deleteGame, addGameController } = require("../controllers/games");
-const { getAllGames } = require('../middlewares/games');
+const { getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, findGameById, deleteGame } = require("../middlewares/games");
+const { sendAllGames, sendUpdatedGames } = require("../controllers/games");
 
-gamesRouter.post("/games", getAllGames, addGameController) 
 gamesRouter.get("/games", getAllGames, sendAllGames);
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
+gamesRouter.post("/games", getAllGames, checkIsTitleInArray, updateGamesArray, updateGamesFile, sendUpdatedGames);
+gamesRouter.delete("/games/:id", getAllGames, findGameById, deleteGame, updateGamesFile, sendUpdatedGames);
 
 module.exports = gamesRouter;
